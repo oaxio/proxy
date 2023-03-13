650a9a8dd95bbaa4d1f7280ecc3794d31971ddce
oaxio <royvroon@hotmail.com>
2023-03-13 14:19:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/api', createProxyMiddleware({
         proxyReq.setHeader('Content-Type', 'application/json-patch+json')
         proxyReq.setHeader('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQkgtMDAwMSIsInVzZXJuYW1lIjoiQkgtMDAwMSIsInBob25lbm8iOiJCSC0wMDAxIiwic3RhZmZpZCI6IjM3YzgyZDYwLThiM2ItNDE5Ni1iYThhLTZjNWU1ZGE1ZjNlNyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6InVzZXIiLCJleHAiOjE2Nzg4ODMyNjAsImlzcyI6ImFjY2Vzc1Rva2VuIn0.ud7TVVBYI5LzMfAi-GvECNjJ-MzBKilfj4dP0sT3xS0')
         console.log("HOI")
-        console.log(req)
+        console.log(req.body)
     },
  }));
 
